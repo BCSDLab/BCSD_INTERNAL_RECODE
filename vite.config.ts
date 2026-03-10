e84933d8be94e099b4b24a9a9cc5c2337c47e0a7
박성빈 <psb106305@gmail.com>
2026-03-10 14:23:03 +0900

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,7 +6,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
+    proxy: {
+      "/v1": "http://localhost:8000",
+    },
   },
 });
